d065e1f62f2d7dd532a264558d81a264059b2894
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-29 18:14:05 +0000

    Automatically backed up by Learn

--- db/migrate/20190629181032_create_github_repos.rb
+++ db/migrate/20190629181032_create_github_repos.rb
@@ -2,7 +2,7 @@ class CreateGithubRepos < ActiveRecord::Migration[5.0]
   def change
     create_table :github_repos do |t|
       t.string :name
-      t.string :html_url
+      t.string :url
     end
   end
 end
