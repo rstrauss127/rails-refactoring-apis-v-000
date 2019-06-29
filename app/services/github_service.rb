c5d2961de2ef68c9c6bf1026c6938edb17407fb4
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-29 18:28:14 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,4 +1,15 @@
 class GithubService
-  def authenticate!()
+
+  def authenticate!(client_id, client_secret, code)
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret,code: code}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    session[:token] = access_hash["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+
+    redirect_to '/'
   end
+
 end
