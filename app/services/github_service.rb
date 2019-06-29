f8357390fb4bfed7fbc8b3bc9acee5621d8bd15a
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-29 19:20:32 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -19,6 +19,6 @@ class GithubService
 
   def get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
-    body = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 end
